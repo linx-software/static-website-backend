b941876e4a6364c2c725175cc8e4829005b5c694
Franz <rodenacker@gmail.com>
2021-05-25 12:32:24 +0200

    Initial
    V1 upload

---
+++ README.md
@@ -0,0 +1,67 @@
+# Linx REST Service as Static Website Backend
+
+## Overview
+
+This sample shows how a Linx REST Service can be used as the backend to a static website. 
+It allows for user registrations, logins and user info updates. 
+The website uses Javascript to communicate with the REST service and does not require any server-side scripting. 
+
+The provided sample includes:
+
+- Linx Solution
+- Website (HTML, JS and CSS)
+- MySQL Database
+
+---
+
+## Dependencies
+
+### Pre-requisites
+
+- Linx Designer
+- MySQL Database Server
+
+### Linx Designer
+
+This solution was developed in the Linx Designer `v5.21.0.0`
+
+### Database
+
+This sample uses an ODBC to connect to the MySQL Database
+
+---
+
+## Setting up the sample
+
+Database
+
+1. Run SQL scripts
+2, Setup ODBC
+
+Linx
+
+1. Open Solution
+2. Change ConnectionString Setting
+3. Debug WebSiteBackendRESTHost Service
+
+Website
+
+1. Copy website files to a folder of your choice
+2a. Right-click on the index.html file and open in a browser of your choice
+OR
+2b. Browse to the website using a local webserver
+
+---
+
+## Using the sample
+
+---
+
+Using the website
+Description: The example website includes user registration, login and updating user info
+
+Usage:
+
+1. Register a user
+2. Login to site
+3. Change user info
---
+++ site/js/index.js
@@ -0,0 +1,241 @@
+/*Vars*/
+var linxapi = "http://localhost:8062";
+var searchString = new URLSearchParams(location.search);
+var token = searchString.getAll("usertoken").toString();
+
+/*Init*/
+const msg = searchString.getAll("msg").toString();
+if (msg != "") {
+    removeFromQuerystring("msg");
+    showMessage(msg);
+}
+
+const registerButton = document.getElementById("registerButton");
+registerButton.addEventListener("click", function () {
+    registerUser();
+});
+
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", function () {
+    loginUser();
+});
+
+const updateUserButton = document.getElementById("updateUserButton");
+updateUserButton.addEventListener("click", function () {
+    updateUser();
+});
+
+pageInit();
+
+/*Page*/
+function pageInit() {
+    document.getElementById("userUpdateContainer").classList.remove("showBlock");
+    var arrEl = document.getElementsByClassName("contentContainer");
+    for (i = 0; i < arrEl.length; i++) {
+        arrEl[i].classList.remove("showBlock");
+    }
+    if (token != "") {
+        getUser();
+        showMessage("You are logged in");
+    } else {
+        showLoginForm();
+        showRegisterForm();
+    }
+}
+function showLoginForm() {
+    const loginContainer = document.getElementById("loginContainer");
+    loginContainer.classList.add("showBlock");
+}
+function showRegisterForm() {
+    const loginContainer = document.getElementById("registerContainer");
+    loginContainer.classList.add("showBlock");
+}
+function showUpdateForm(userData) {
+    document.getElementById("userFirstName").value = userData.FirstName;
+    document.getElementById("userLastName").value = userData.LastName;
+    document.getElementById("userEmail").value = userData.Email;
+    document.getElementById("userPassword").value = userData.Password;
+    document.getElementById("userUpdateContainer").classList.add("showBlock");
+}
+/*API CRUD*/
+function registerUser() {
+    let registerFirstName = document.getElementById("registerFirstName").value;
+    let registerLastName = document.getElementById("registerLastName").value;
+    let registerEmail = document.getElementById("registerEmail").value;
+    let registerPassword = document.getElementById("registerPassword").value;
+    if (registerFirstName == "" || registerLastName == "" || registerEmail == "" || registerPassword == "") {
+        showErrorMsg("registerContainer", "All fields are required");
+    } else {
+        removeErrorMsg();
+        const postJson = { User: { FirstName: registerFirstName, LastName: registerLastName, Email: registerEmail, Password: registerPassword } };
+        postData("/register", postJson)
+            .then(status)
+            .then(json)
+            .then((data) => {
+                showMessage(data.Message);
+            })
+            .catch((error) => {
+                showAPIError(error);
+            });
+    }
+}
+function loginUser() {
+    const loginEmail = document.getElementById("loginEmail").value;
+    const loginPassword = document.getElementById("loginPassword").value;
+    const postJson = { Email: loginEmail, Password: loginPassword };
+    if (loginEmail == "") {
+        showErrorMsg("loginContainer", "Email required");
+    } else if (loginPassword == "") {
+        showErrorMsg("loginContainer", "Password required");
+    } else {
+        removeErrorMsg();
+        postData("/login", postJson)
+            .then(status)
+            .then(json)
+            .then((data) => {
+                if (data.Success) {
+                    token = data.Token;
+                    addToQuerystring('usertoken',token);
+                    pageInit();
+                } else {
+                    showMessage(data.Message);
+                }
+            })
+            .catch((error) => {
+                showAPIError(error);
+            });
+    }
+}
+function getUser() {
+    getData("/getuser")
+        .then(status)
+        .then(json)
+        .then((data) => {
+            showUpdateForm(data.User);
+        })
+        .catch((error) => {
+            showAPIError(error);
+        });
+}
+function updateUser() {
+    let userFirstName = document.getElementById("userFirstName").value;
+    let userLastName = document.getElementById("userLastName").value;
+    let userEmail = document.getElementById("userEmail").value;
+    let userPassword = document.getElementById("userPassword").value;
+    if (userFirstName == "" || userLastName == "") {
+        showErrorMsg("userUpdateContainer", "FirstName and LastName are required");
+    } else {
+        removeErrorMsg();
+        const postJson = { User: { FirstName: userFirstName, LastName: userLastName, Password: userPassword } };
+        postData("/updateuser", postJson)
+            .then(status)
+            .then(json)
+            .then((data) => {
+                showMessage(data.Message);
+                userFirstName.value = data.User.FirstName;
+                userLastName.value = data.User.LastName;
+                userEmail.value = data.User.Email;
+                userPassword.value = "";
+            })
+            .catch((error) => {
+                showAPIError(error);
+            });
+    }
+}
+/*API Common*/
+async function postData(url = "", data = {}) {
+    const response = await fetch(linxapi + url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data),
+    });
+    return response;
+}
+async function getData(url = "") {
+    const response = await fetch(linxapi + url, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+    });
+    return response;
+}
+function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response);
+    } else if (response.status === 401) {
+        window.location.href = "index.html?msg=Please login";
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+function json(response) {
+    return response.json();
+}
+/*Utils*/
+function showMessage(message) {
+    let msgContainer = document.getElementById("msgContainer");
+    if (msgContainer) {
+        msgContainer.remove();
+    }
+    msgContainer = document.createElement("div");
+    msgContainer.setAttribute("id", "msgContainer");
+    const msg = document.createElement("span");
+    msg.innerHTML = message;
+    msgContainer.appendChild(msg);
+    document.getElementsByTagName("body")[0].appendChild(msgContainer);
+    setTimeout(function () {
+        msgContainer.classList.add("visible");
+    }, 500);
+}
+function showErrorMsg(parent, msg) {
+    removeErrorMsg();
+    const parentContainer = document.getElementById(parent);
+    const errorMsgContainer = document.createElement("span");
+    errorMsgContainer.classList.add("errorMsg");
+    const textNode = document.createTextNode(msg);
+    errorMsgContainer.appendChild(textNode);
+    parentContainer.appendChild(errorMsgContainer);
+}
+function removeErrorMsg() {
+    var arrEl = document.getElementsByClassName("errorMsg");
+    for (i = 0; i < arrEl.length; i++) {
+        arrEl[i].remove();
+    }
+}
+function showAPIError(msg) {
+    console.log(msg);
+    showMessage(msg);
+}
+function showPassword(id) {
+    const el = document.getElementById(id);
+    if (el.type === "password") {
+        el.type = "text";
+    } else {
+        el.type = "password";
+    }
+}
+function addToQuerystring(key,val){
+    const url = new URL(window.location);
+    url.searchParams.set(key,val);
+    window.history.pushState({}, '', url);
+}
+function removeFromQuerystring(key){
+    const url = new URL(window.location);
+    url.searchParams.delete(key);
+    window.history.pushState({}, '', url);
+}
